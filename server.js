de7a4e32809fbd2e7a5a919d468b02bcc718e3bd
Alexander24117 <60900058+Alexander24117@users.noreply.github.com>
2021-11-22 21:39:27 -0500

    route

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/clienteangularae'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/clienteangular'));
+
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/clienteangularae/principal.html'));});
+    
+res.sendFile(path.join(__dirname+'/dist/clienteangular/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
